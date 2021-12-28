9d24f60237a96b1a48221bd38505de2fc832f8a7
hnouh <haninnouh@gmail.com>
2021-12-28 11:58:39 +0300

    first commit

--- redux-middleware/src/pages/ReduxEx.js
+++ redux-middleware/src/pages/ReduxEx.js
@@ -1,47 +1,52 @@
 import { incCounter, decCounter, resetCounter } from "../reducers/counter";
 import { useDispatch, useSelector } from "react-redux";
-import {fetchData} from '../reducers/posts'
+import { fetchData } from "../reducers/posts";
 import { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 
 const ReduxEx = () => {
-    const dispatch = useDispatch();
-    // const [posts,setPosts]=useState([])
-    // const [loading,setLoading]=useState(true)
+  const dispatch = useDispatch();
+  // const [posts,setPosts]=useState([])
+  // const [loading,setLoading]=useState(true)
 
-  useEffect(() =>
-        dispatch(fetchData)
-    ,[])
+  useEffect(() => dispatch(fetchData), []);
+
+  // useEffect (() => {
+  //     axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
+  //         setPosts(res.data)
+  //     })
+  //   },[]);
 
   const state = useSelector((store) => {
-      // console.log(store)
+    // console.log(store)
     return {
       counter: store.counter.counter,
-      posts:store.posts.posts
+      posts: store.posts.posts,
     };
   });
 
   return (
     <>
       <br></br>
-      <button onClick={()=> dispatch(incCounter(5))} >+</button>
+      <button onClick={() => dispatch(incCounter(5))}>+</button>
       {/* <button onClick={() => dispatch(incCounter())}>+</button> */}
       <p>{state.counter}</p>
-      <button onClick={()=>dispatch(decCounter(5))} >-</button><br></br>
+      <button onClick={() => dispatch(decCounter(5))}>-</button>
+      <br></br>
       {/* <button onClick={() => dispatch(decCounter())}>-</button> */}
       <br></br>
       <button onClick={() => dispatch(resetCounter())}>Reset Counter</button>
       <br></br>
       <br></br>
 
-    {state.posts.map((post)=>{
-      return(
-        <>
-        <h3>{post.userId}</h3>
-        <p>{post.body}</p>
-        </>
-      )
-    })}
+      {state.posts.map((post) => {
+        return (
+          <>
+            <h3>{post.userId}</h3>
+            <p>{post.body}</p>
+          </>
+        );
+      })}
     </>
   );
 };
